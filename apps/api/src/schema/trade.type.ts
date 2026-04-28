b453e3138352cc52fed66120fc68a2c5eba93dce
krishna joshi <krishnajoshi4698@gmail.com>
2026-04-29 04:23:26 +0530

    close orderaadd type

--- apps/api/src/schema/trade.type.ts
+++ apps/api/src/schema/trade.type.ts
@@ -2,11 +2,16 @@ import {z} from "zod"
 
 export const createOrderbodySchema = z.object({
      status: z.enum(["open", "closed"]),
-     type:z.enum(["long", "short"]),
-     levrage:z.coerce.number(),
-     qty:z.coerce.number(),
+     side:z.enum(["long", "short"]),
+     leverage:z.coerce.number().positive(),
+     qty:z.coerce.number().positive(),
      asset:z.string(),
      takeprofit:z.coerce.number().optional(),
      stoploss:z.coerce.number().optional()
 })
  export type createOrderBody= z.infer< typeof createOrderbodySchema>
+
+ export const  closeOrder= z.object({
+     
+      orderId:z.string(),
+ })
