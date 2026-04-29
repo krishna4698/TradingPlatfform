c1e7ab69323b5b0175208a17b72beaab0fe55fda
krishna joshi <krishnajoshi4698@gmail.com>
2026-04-30 00:20:22 +0530

    updated

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -10,22 +10,23 @@ const app = express();
 const port = Number(process.env.PORT ?? 3001);
 const frontendUrl = process.env.FRONTEND_URL ?? "http://localhost:3000";
 app.use(cookieParser())
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", frontendUrl);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", frontendUrl);
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(204);
+//   }
 
-  next();
-});
+//   next();
+// });
 
 app.use(cors({
   origin: [
     "https://your-frontend.vercel.app",
+    frontendUrl,
      "http://localhost:3000"
   ],
   credentials: true
