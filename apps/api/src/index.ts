fc29b652c53c8d4b4465c90b5535d815d4b01285
krishna joshi <krishnajoshi4698@gmail.com>
2026-04-30 00:00:10 +0530

    added cors issues

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser"
-import { prisma } from "@repo/db";
+import cors from "cors"
 
 import tradeRouter from  "./routes/trade.route.js"
 import authRouter from "./routes/auth.route.js"
@@ -8,9 +8,10 @@ import balanceRouter from "./routes/balance.route.js"
 import getCandles from "./routes/candle.route.js"
 const app = express();
 const port = Number(process.env.PORT ?? 3001);
+const frontendUrl = process.env.FRONTEND_URL ?? "http://localhost:3000";
 app.use(cookieParser())
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", frontendUrl);
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
@@ -21,6 +22,14 @@ app.use((req, res, next) => {
 
   next();
 });
+
+app.use(cors({
+  origin: [
+    "https://your-frontend.vercel.app",
+     "http://localhost:3000"
+  ],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use("/auth", authRouter)
